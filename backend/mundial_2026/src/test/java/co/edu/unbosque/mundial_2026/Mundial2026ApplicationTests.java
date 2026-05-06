ff8e5c0dfbfd1981daf9d47be87911c6980f04c5
jsbecerrap <jsbecerrap@unbosque.edu.co>
2026-05-06 12:27:28 -0500

    fix: configurar H2 para tests de contexto

--- backend/mundial_2026/src/test/java/co/edu/unbosque/mundial_2026/Mundial2026ApplicationTests.java
+++ backend/mundial_2026/src/test/java/co/edu/unbosque/mundial_2026/Mundial2026ApplicationTests.java
@@ -2,8 +2,10 @@ package co.edu.unbosque.mundial_2026;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
 class Mundial2026ApplicationTests {
 
 	@Test
